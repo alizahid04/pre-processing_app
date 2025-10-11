1f5f545dc2353c66fe1ec53b823f7bb0046644c7
Muhammad Ali <alizahid272004@gmail.com>
2025-10-11 23:28:50 +0500

    Fix port for Back4App deployment

--- app_flutter.py
+++ app_flutter.py
@@ -101,7 +101,7 @@ def process_text():
     return jsonify({"result": result})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
 
 
